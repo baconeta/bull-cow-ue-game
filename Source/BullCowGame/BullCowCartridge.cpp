d602364e56b5d3132ca5e047300c7003bac18293
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-11-19 19:15:04 +0200

    update intro message

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome!");
+    PrintLine("Welcome to the Bull Cow Game!");
+    PrintLine("If you don't know how to play, too bad.\nThere are no instructions yet.");
+    PrintLine("Please input something into the window.");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
