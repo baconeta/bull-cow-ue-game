b8fb5dc6417e42352a2cf58983610d79e1b7b7f2
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-11-19 21:32:18 +0200

    pseudocode

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,19 +4,33 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    HiddenWord = TEXT("marsh"); // Set the hidden word
+    LivesRemaining = 5; // Set the lives
+
     PrintLine(TEXT("Welcome to the Bull Cow Game!"));
     PrintLine(TEXT("If you don't know how to play, too bad.\nThere are no instructions yet."));
-    PrintLine(TEXT("Please input something into the window."));
+    PrintLine(TEXT("Please guess the 5 letter word:")); // TODO change 5 to magic number 
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    FString HiddenWord = TEXT("marsh");
     ClearScreen();
     if (Input == HiddenWord) {
         PrintLine(TEXT("Congratulations,\nyou guessed the hidden word - well done"));
     } else {        
         PrintLine(Input);
         PrintLine(TEXT("That word is incorrect. Sorry."));
+        
     }
+
+    // check if isogram
+    // check length of word
+    // remove a life
+
+    // check lives > 0
+    // if yes, guess again
+    // show lives remaining
+    // if no, show game over and hidden word
+
 }
