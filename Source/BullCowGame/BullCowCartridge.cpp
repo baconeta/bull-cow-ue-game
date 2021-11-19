5757b2b4d8c248f145de6d81ad252f1149666ee8
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-11-19 21:37:25 +0200

    initialise game function

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,8 +5,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    HiddenWord = TEXT("marsh"); // Set the hidden word
-    LivesRemaining = 5; // Set the lives
+    InitGame();
 
     PrintLine(TEXT("Welcome to the Bull Cow Game!"));
     PrintLine(TEXT("If you don't know how to play, too bad.\nThere are no instructions yet."));
@@ -18,10 +17,10 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     ClearScreen();
     if (Input == HiddenWord) {
         PrintLine(TEXT("Congratulations,\nyou guessed the hidden word - well done"));
-    } else {        
+    }
+    else {        
         PrintLine(Input);
         PrintLine(TEXT("That word is incorrect. Sorry."));
-        
     }
 
     // check if isogram
@@ -32,5 +31,9 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     // if yes, guess again
     // show lives remaining
     // if no, show game over and hidden word
+}
 
+void UBullCowCartridge::InitGame() {
+    HiddenWord = TEXT("marsh"); // Set the hidden word
+    LivesRemaining = 5; // Set the lives
 }
